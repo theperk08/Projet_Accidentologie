e779528c9349f3751502c3937efd69179bcf875f
Manu Musy <manuelmusy8@gmail.com>
2025-01-16 11:41:23 +0100

    update streamlit app with graph on accueil

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,8 +11,11 @@ rep_data = "data/"
 
 df_cyclistes = pd.read_csv(rep_data+"df_cyclistes_2023.csv", sep=";")
 
-# st.dataframe(df_cyclistes)
-
+df_max = df_cyclistes[df_cyclistes['catv'].isin([1, 80])].groupby('id_usager', as_index=False).max()
+df_first = df_cyclistes[df_cyclistes['catv'].isin([1, 80])].groupby('id_usager', as_index=False).first()
+df_nb_vehicules = df_cyclistes.groupby('Num_Acc', as_index=False)['id_vehicule'].nunique()
+vehicule_multiple = list(df_nb_vehicules[df_nb_vehicules['id_vehicule']>1]['Num_Acc'].unique())
+cycliste_seul = list(df_nb_vehicules[df_nb_vehicules['id_vehicule']==1]['Num_Acc'].unique())
 
 
 def accueil():
@@ -33,14 +36,202 @@ def accueil():
     
     if selection == "Accueil":
         st.write('Bienvenue')
+        page_accueil()
+
         #st.image(rep_images+"home.jpg")
     elif selection == "Visualisations des statistiques":
-        visu()
-  
-df_max = df_cyclistes[df_cyclistes['catv'].isin([1, 80])].groupby('id_usager', as_index=False).max()
-df_first = df_cyclistes[df_cyclistes['catv'].isin([1, 80])].groupby('id_usager', as_index=False).first()
+        page_visu()  
 
-def visu():
+
+def page_accueil():
+    
+    # cyclistes
+    st.header("Statistiques 2023 sur la gravité des accidents corporels de la circulation impliquant des cyclistes (accidents enregistrés en France, outre-mer compris)")
+    
+    # cyclistes sans autre véhicule
+    
+    df1 = df_cyclistes[(df_cyclistes['Num_Acc'].isin(cycliste_seul)) & (df_cyclistes['catv'].isin([1, 80]))].groupby('id_usager', as_index=False).first()
+
+    fig1 = px.histogram(df1, x='gravity',
+                    color='gravity',
+                    color_discrete_map = {'Indemne':'green', 'Blessé léger': 'orange', 'Blessé hospitalisé': 'red', 'Tué': 'black' },
+                    category_orders={'gravity':['Indemne', 'Blessé léger', 'Blessé hospitalisé', 'Tué']},
+                    title="Cyclistes impliqués sans autre véhicule",
+                    text_auto=False)
+
+    # Calcul des pourcentages
+    counts = df1['gravity'].value_counts()
+    total = len(df1)
+    percentages = (counts / total * 100).sort_index()
+
+    # Ajout des annotations de pourcentages
+    for cat, percentage in percentages.items():
+        fig1.add_annotation(
+            x=cat,
+            y=counts[cat],  # Hauteur correspondante
+            text=f"{percentage:.1f}%",
+            showarrow=False,
+            yshift=10,  # Décalage vertical pour la lisibilité
+            font=dict(color="black", size=12)
+        )
+
+    # Mise en forme
+    fig1.update_traces(textfont_size=12, textposition='outside', hovertemplate='%{x}<br>%{y}<br>',
+                    marker_line_color= '#800020', marker_line_width=1.5,
+                    opacity=0.8)
+    fig1.update_layout(width=600, height=400,
+                    yaxis_title="Nombre",
+                    # xaxis_title="Catégorie",
+                    xaxis_title=None,
+                    showlegend=True,
+                    title = dict(font=dict(size=15), x=0.5)
+                    )
+    st.plotly_chart(fig1)
+
+    # Cyclistes avec autres véhicules
+    
+    df2 = df_cyclistes[(df_cyclistes['Num_Acc'].isin(vehicule_multiple)) & (df_cyclistes['catv'].isin([1, 80]))].groupby('id_usager', as_index=False).first()
+
+    fig2 = px.histogram(df2, x='gravity',
+                    color='gravity',
+                    color_discrete_map = {'Indemne':'green', 'Blessé léger': 'orange', 'Blessé hospitalisé': 'red', 'Tué': 'black' },
+                    category_orders={'gravity':['Indemne', 'Blessé léger', 'Blessé hospitalisé', 'Tué']},
+                    title="Cyclistes impliqués avec un autre véhicule",
+                    text_auto=False)
+
+    # Calcul des pourcentages
+    counts = df2['gravity'].value_counts()
+    total = len(df2)
+    percentages = (counts / total * 100).sort_index()
+
+    # Ajout des annotations de pourcentages
+    for cat, percentage in percentages.items():
+        fig2.add_annotation(
+            x=cat,
+            y=counts[cat],  # Hauteur correspondante
+            text=f"{percentage:.1f}%",
+            showarrow=False,
+            yshift=10,  # Décalage vertical pour la lisibilité
+            font=dict(color="black", size=12)
+        )
+
+    # Mise en forme
+    fig2.update_traces(textfont_size=12, textposition='outside', hovertemplate='%{x}<br>%{y}<br>',
+                    marker_line_color= '#800020', marker_line_width=1.5,
+                    opacity=0.8)
+    fig2.update_layout(width=600, height=400,
+                    yaxis_title="Nombre",
+                    # xaxis_title="Catégorie",
+                    xaxis_title=None,
+                    showlegend=True,
+                    title = dict(font=dict(size=15), x=0.5)
+                    )
+    st.plotly_chart(fig2)
+
+    
+    # autres usagers
+    df3 = df_cyclistes[~df_cyclistes['catv'].isin([1, 80])].groupby('id_usager', as_index=False).first()
+
+    
+    fig3 = px.histogram(df3, x='gravity',
+                    color='gravity',
+                    color_discrete_map = {'Indemne':'green', 'Blessé léger': 'orange', 'Blessé hospitalisé': 'red', 'Tué': 'black' },
+                    category_orders={'gravity':['Indemne', 'Blessé léger', 'Blessé hospitalisé', 'Tué']},
+                    title="Autres usagers impliqués avec des cyclistes", text_auto=False)
+
+    # Calcul des pourcentages
+    counts = df3['gravity'].value_counts()
+    total = len(df3)
+    percentages = (counts / total * 100).sort_index()
+
+    # Ajout des annotations de pourcentages
+    for cat, percentage in percentages.items():
+        fig3.add_annotation(
+            x=cat,
+            y=counts[cat],  # Hauteur correspondante
+            text=f"{percentage:.1f}%",
+            showarrow=False,
+            yshift=10,  # Décalage vertical pour la lisibilité
+            font=dict(color="black", size=12)
+        )
+
+    # Mise en forme
+    fig3.update_traces(textfont_size=12, textposition='outside', hovertemplate='%{x}<br>%{y}<br>',
+                    marker_line_color= '#800020', marker_line_width=1.5,
+                    opacity=0.8)
+    fig3.update_layout(width=600, height=400,
+                    yaxis_title="Nombre",
+                    # xaxis_title="Catégories",
+                    xaxis_title=None,
+                    showlegend=True,
+                    legend_title_text="",
+                    title = dict(font=dict(size=15), x=0.5)
+                    )
+    st.plotly_chart(fig3)
+
+
+    # Par type d'endroit
+    df4 = df_max
+    fig4 = px.histogram(df4, x='situ', title="Cyclistes par type d'endroit", text_auto=False)
+
+    # Calcul des pourcentages
+    counts = df4['situ'].value_counts()
+    total = len(df4)
+    percentages = (counts / total * 100).sort_index()
+
+    # Ajout des annotations de pourcentages
+    for cat, percentage in percentages.items():
+        fig4.add_annotation(
+            x=cat,
+            y=counts[cat],  # Hauteur correspondante
+            text=f"{percentage:.1f}%",
+            showarrow=False,
+            yshift=10,  # Décalage vertical pour la lisibilité
+            font=dict(color="black", size=12)
+        )
+
+    # Mise en forme
+    fig4.update_traces(textfont_size=12, textposition='outside', hovertemplate='%{x}<br>%{y}<br>',
+                    marker_line_color= '#800020', marker_line_width=1.5,
+                    opacity=0.8)
+    fig4.update_xaxes(tickmode="array", tickvals = [-1,0,1,2,3,4,5, 6, 8], ticktext= ['Inconnu','Aucun','Sur chaussée',  'Sur b.a.u', 'Sur accotement', 'Sur trottoir', 'Sur piste cyclable', 'Sur autre voie spéciale', 'Autres'])
+
+    fig4.update_layout(width=800, height=400,
+                    yaxis_title="Nombre",
+                    # xaxis_title="Catégories",
+                    xaxis_title=None,
+                    showlegend=True,
+                    legend_title_text="",
+                    title = dict(font=dict(size=15), x=0.5)
+    )
+
+    fig4.update_layout(title='Cyclistes impliqués par type d\'endroit')
+    st.plotly_chart(fig4)
+
+    df6 = df_max
+    fig6 = px.histogram(df6, x='situ', title="Cyclistes par type d'endroit",
+                    color='gravity',
+                    color_discrete_map = {'Indemne':'green', 'Blessé léger': 'orange', 'Blessé hospitalisé': 'red', 'Tué': 'black' },
+                    category_orders={'gravity':['Indemne', 'Blessé léger', 'Blessé hospitalisé', 'Tué']},
+                    text_auto=False)
+
+    fig6.update_xaxes(tickmode="array", tickvals = [-1,0,1,2,3,4,5, 6, 8], ticktext= ['Inconnu','Aucun','Sur chaussée',  'Sur b.a.u', 'Sur accotement', 'Sur trottoir', 'Sur piste cyclable', 'Sur autre voie spéciale', 'Autres'])
+
+    fig6.update_layout(width=800, height=400,
+                    yaxis_title="Nombre",
+                    # xaxis_title="Catégories",
+                    xaxis_title=None,
+                    showlegend=True,
+                    legend_title_text="",
+                    title = dict(font=dict(size=15), x=0.5)
+                    )
+
+    fig6.update_layout(title='Gravité Cyclistes impliqués par type d\'endroit')
+    st.plotly_chart(fig6)
+
+
+
+def page_visu():
     cols = ['']
     target = "gravity"
 
